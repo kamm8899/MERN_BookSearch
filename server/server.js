06a4ef996f327ed8912fa9b45500424fd8c0b5f6
kamm8899 <kamm8899@gmail.com>
2021-11-12 13:51:30 -0800

    updated server.js

--- server/server.js
+++ server/server.js
@@ -8,34 +8,24 @@ const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require('./utils/auth');
 // const routes = require('./routes');
 
-const startServer = async () => {
-
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware
   });
 
-    // Start the Apollo server
-    await server.start();
       // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
     // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  };
 
 
-// Initialize the Apollo server
-startServer();
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
+
 
 
 // if we're in production, serve client/build as static assets
@@ -46,3 +36,11 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
+
+//file is updated
