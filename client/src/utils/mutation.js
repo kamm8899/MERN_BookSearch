c745da5721b346829426bee25f7cd14ae6420133
kamm8899 <kamm8899@gmail.com>
2021-11-13 18:42:51 -0800

    added mutations in client for addUser, Save Book and login user

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,50 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+
+export const SAVE_BOOK = gql`
+  mutation save_book($input: BookInput!) {
+    saveBook(input: $input) {
+        _id
+        username
+        email
+        saveBooks{
+            bookId
+            title
+            image
+            description
+            link 
+            authors
+        }
+      }
+  }
+`;
+
+export const REMOVE_BOOK = gpl`
+  mutation removeBook()
+`
