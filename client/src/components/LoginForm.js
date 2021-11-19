cefe40313cbfa31ada463570de02b085e3e98c76
kamm8899 <kamm8899@gmail.com>
2021-11-18 18:35:10 -0800

    changed variable

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -31,7 +31,7 @@ const LoginForm = () => {
     try {
       const data = await login({ variables: {...userFormData}
       });
-      Auth.login(response.login.token);
+      Auth.login(data.login.token);
     
     } catch (err) {
       console.error(err);
